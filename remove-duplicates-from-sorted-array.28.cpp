a600960426899f46c5fed33a7c917676d46ecd69
siddmadd <siddmadd@umich.edu>
2024-06-24 15:13:58 -0400

    anoother of the same

---
+++ remove-duplicates-from-sorted-array.28.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        map<int,int> dic;
+        int loc_of_ins = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (dic.find(nums[i]) == dic.end()) {
+                nums[loc_of_ins] = nums[i];
+                ++loc_of_ins;
+                dic[nums[i]] = 0;
+            } else {
+                continue;
+            }
+
+        }
+        return loc_of_ins;
+    }
+};
